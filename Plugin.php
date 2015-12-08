3c8312d06667241078549e57b4e67b9cd622ef08
Frederic Guillot <fred@kanboard.net>
2015-12-07 22:08:05 -0500

    Change deprecated event

--- Plugin.php
+++ Plugin.php
@@ -22,7 +22,7 @@ class Plugin extends Base
         $this->userNotificationType->setType('jabber', t('Jabber'), '\Kanboard\Plugin\Jabber\Notification\Jabber');
         $this->projectNotificationType->setType('jabber', t('Jabber'), '\Kanboard\Plugin\Jabber\Notification\Jabber');
 
-        $this->on('session.bootstrap', function($container) {
+        $this->on('app.bootstrap', function($container) {
             Translator::load($container['config']->getCurrentLanguage(), __DIR__.'/Locale');
         });
     }
@@ -39,7 +39,7 @@ class Plugin extends Base
 
     public function getPluginVersion()
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     public function getPluginHomepage()
