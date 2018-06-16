554d2298b392327fd7a03c5c123e911e952610b5
Balázs Úr <urbalazs@gmail.com>
2018-06-16 17:55:48 +0200

    Updated Hungarian translation

--- Locale/hu_HU/translations.php
+++ Locale/hu_HU/translations.php
@@ -1,12 +1,12 @@
 <?php
 
 return array(
-    // 'Jabber (XMPP)' => '',
-    // 'XMPP server address' => '',
-    // 'Jabber domain' => '',
-    // 'Jabber nickname' => '',
-    // 'Multi-user chat room' => '',
-    // 'Help on Jabber integration' => '',
-    // 'The server address must use this format: "tcp://hostname:5222"' => '',
+    'Jabber (XMPP)' => 'Jabber (XMPP)',
+    'XMPP server address' => 'XMPP kiszolgáló címe',
+    'Jabber domain' => 'Jabber tartomány',
+    'Jabber nickname' => 'Jabber becenév',
+    'Multi-user chat room' => 'Többfelhasználós csevegőszoba',
+    'Help on Jabber integration' => 'Súgó a Jabber integrációhoz',
+    'The server address must use this format: "tcp://hostname:5222"' => 'A kiszolgáló címének ezt a formátumot kell használnia: „tcp://gépnév:5222”',
 );
 
